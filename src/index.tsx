780339cdabdf177444cb82668e517ee0fc5797b0
Thomas Scharke <ask@van.dermeer.de>
2019-12-01 15:08:56 +0100

    feat(quotes): Use animate.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
-import * as React from 'react'
-import { render } from 'react-dom'
-import Quote from './Quote'
+const INTERVAL_IN_MS = 8000
 
-render(
-  <Quote delay={10000} />,
-  document.getElementById('quoteElement'),
-)
+;(() => {
+  const allElements = document.querySelectorAll('p.quote')
+  const lengthOfAllElements = allElements.length -1
+  let nextIndex = 1
+  
+  setInterval(() => {
+    (document.querySelector('p.quote:not(.hidden)') as any).classList.add('hidden')
+    allElements[nextIndex].classList.remove('hidden')
+
+    nextIndex = nextIndex === lengthOfAllElements ? 0 : nextIndex + 1
+  }, INTERVAL_IN_MS)
+})()
